31b30301516c26571d37fdde4c4da8cbe55a0909
VishnuSanal <t.v.s10123@gmail.com>
2020-04-10 16:11:23 +0530

    Updated About Page and Optimised code

--- app/src/main/java/phone/vishnu/quotes/fragment/BlankFragment.java
+++ app/src/main/java/phone/vishnu/quotes/fragment/BlankFragment.java
@@ -1,16 +1,21 @@
 package phone.vishnu.quotes.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import phone.vishnu.quotes.R;
 
 public class BlankFragment extends Fragment {
+    private TextView sourceCodeTV, feedbackTV;
 
     public BlankFragment() {
     }
@@ -21,6 +26,48 @@ public class BlankFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        View inflate = inflater.inflate(R.layout.fragment_blank, container, false);
+        sourceCodeTV = inflate.findViewById(R.id.aboutPageViewSourceCodeTextView);
+        feedbackTV = inflate.findViewById(R.id.aboutPageFeedbackTextView);
+        return inflate;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        sourceCodeTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uriUrl = Uri.parse("https://github.com/VishnuSanal/Quotes");
+                startActivity(new Intent(Intent.ACTION_VIEW, uriUrl));
+            }
+        });
+        feedbackTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                composeEmail(new String[]{getActivity().getString(R.string.email_address_of_developer)}, "Feedback of " + getActivity().getString(R.string.app_name));
+
+
+//                Intent intent = new Intent(Intent.ACTION_SEND);
+//                intent.setType("message/rfc822");
+//                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{getActivity().getString(R.string.email_address_of_developer)});
+//                intent.putExtra(Intent.EXTRA_SUBJECT, "Feedback of " + getActivity().getString(R.string.app_name));
+//                startActivity(Intent.createChooser(intent, "Send Feedback:"));
+
+
+            }
+        });
+    }
+
+    private void composeEmail(String[] addresses, String subject) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
