6c32fedbf5e468887ec6c54c4a47127b966faba0
Vishnu Sanal. T <t.v.s10123@gmail.com>
2020-11-06 19:49:05 +0530

    Performance Improvements v1.7.3

--- app/src/main/java/phone/vishnu/quotes/data/QuoteData.java
+++ app/src/main/java/phone/vishnu/quotes/data/QuoteData.java
@@ -1,8 +1,12 @@
 package phone.vishnu.quotes.data;
 
+import com.android.volley.Cache;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.google.firebase.crashlytics.FirebaseCrashlytics;
 
@@ -10,6 +14,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 import phone.vishnu.quotes.controller.AppController;
@@ -58,7 +63,56 @@ public class QuoteData {
                                 error.printStackTrace();
                             }
                         }
-                );
+                ) {
+                    @Override
+                    protected Response<JSONArray> parseNetworkResponse(NetworkResponse response) {
+                        try {
+                            Cache.Entry cacheEntry = HttpHeaderParser.parseCacheHeaders(response);
+                            if (cacheEntry == null) {
+                                cacheEntry = new Cache.Entry();
+                            }
+                            final long cacheHitButRefreshed = 1000;
+                            final long cacheExpired = 7 * 24 * 60 * 60 * 1000;
+                            long now = System.currentTimeMillis();
+                            final long softExpire = now + cacheHitButRefreshed;
+                            final long ttl = now + cacheExpired;
+                            cacheEntry.data = response.data;
+                            cacheEntry.softTtl = softExpire;
+                            cacheEntry.ttl = ttl;
+                            String headerValue;
+                            headerValue = response.headers.get("Date");
+                            if (headerValue != null) {
+                                cacheEntry.serverDate = HttpHeaderParser.parseDateAsEpoch(headerValue);
+                            }
+                            headerValue = response.headers.get("Last-Modified");
+                            if (headerValue != null) {
+                                cacheEntry.lastModified = HttpHeaderParser.parseDateAsEpoch(headerValue);
+                            }
+                            cacheEntry.responseHeaders = response.headers;
+                            final String jsonString = new String(response.data,
+                                    HttpHeaderParser.parseCharset(response.headers));
+                            return Response.success(new JSONArray(jsonString), cacheEntry);
+                        } catch (UnsupportedEncodingException | JSONException e) {
+                            return Response.error(new ParseError(e));
+                        }
+                    }
+
+                    @Override
+                    protected void deliverResponse(JSONArray response) {
+                        super.deliverResponse(response);
+                    }
+
+                    @Override
+                    public void deliverError(VolleyError error) {
+                        super.deliverError(error);
+                    }
+
+                    @Override
+                    protected VolleyError parseNetworkError(VolleyError volleyError) {
+                        return super.parseNetworkError(volleyError);
+                    }
+
+                };
                 AppController.getInstance().addToRequestQueue(jsonArrayRequest);
             }
         });
