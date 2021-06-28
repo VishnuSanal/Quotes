250e9063d4bdfc622201651a877489fab1718883
Vishnu Sanal. T <t.v.s10123@gmail.com>
2021-06-28 17:25:17 +0530

    Updated Welcome Tour

--- app/src/main/java/phone/vishnu/quotes/adapter/TourFragmentStateAdapter.java
+++ app/src/main/java/phone/vishnu/quotes/adapter/TourFragmentStateAdapter.java
@@ -38,14 +38,13 @@ public class TourFragmentStateAdapter extends androidx.viewpager2.adapter.Fragme
         ArrayList<TourItem> tourItems = new ArrayList<>();
 
         tourItems.add(new TourItem(R.drawable.ic_quotes, "Spread positivity with us", "Would you try?"));
-        tourItems.add(new TourItem(R.drawable.ic_quotes_round, "It Works Like...", "Share quotations as an HD Image without any hassles of image editing"));
-        tourItems.add(new TourItem(R.drawable.ic_unfold_more, "Scroll For More", "Scroll horizontally to get more Quotes"));
-        tourItems.add(new TourItem(R.drawable.ic_insert_photo, "Background Image", "Customize Background with Images or Plain colour"));
+        tourItems.add(new TourItem(R.drawable.ic_quotes_round, "It Works Like...", "Quotes Status Creator lets you share quotations as status images on social media"));
+        tourItems.add(new TourItem(R.drawable.ic_insert_photo, "Background Image", "Customize Background with Custom Background Images, Solid Background Colors or Images from Gallery"));
         tourItems.add(new TourItem(R.drawable.ic_color_lens, "Color", "Customizable Card & Font Colours"));
-        tourItems.add(new TourItem(R.drawable.ic_font, "Font", "Customizable Font"));
+        tourItems.add(new TourItem(R.drawable.ic_font, "Font", "Custom Font Styles"));
         tourItems.add(new TourItem(R.drawable.ic_search, "Search", "Search for Quotes"));
-        tourItems.add(new TourItem(R.drawable.ic_notifications, "Daily Notification of Quotes", "Daily Notifications with inspiring Quotes"));
-        tourItems.add(new TourItem(R.drawable.ic_share, "Share Quotes in Social Media", "Share Quote as an HD Image"));
+        tourItems.add(new TourItem(R.drawable.ic_notifications, "Daily Notification of Quotes", "Get notified with your daily dose of motivation"));
+        tourItems.add(new TourItem(R.drawable.ic_share, "Share Quotes as Image", "Share Quote as an HD Image"));
         tourItems.add(new TourItem(R.drawable.ic_copy, "Share Quotes as Text", "Copy Quotes to Clipboard"));
         tourItems.add(new TourItem(R.drawable.ic_save, "Save Quotes to Gallery", "Save Quotes to Gallery as an HD Image"));
         tourItems.add(new TourItem(R.drawable.ic_favorite, "Favorite Quotes", "Add to Favourites for easy-access of Quotes"));
