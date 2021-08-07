5e1b8682e965cd98a3de0c0dc7468860a902da90
Vishnu Sanal. T <t.v.s10123@gmail.com>
2021-08-07 15:46:30 +0530

    Fixes #21

--- app/src/main/java/phone/vishnu/quotes/fragment/BGImagePickFragment.java
+++ app/src/main/java/phone/vishnu/quotes/fragment/BGImagePickFragment.java
@@ -32,11 +32,14 @@ import phone.vishnu.quotes.activity.MainActivity;
 import phone.vishnu.quotes.adapter.BGImageRVAdapter;
 import phone.vishnu.quotes.helper.SharedPreferenceHelper;
 
-public class BGImagePickFragment extends BottomSheetDialogFragment {
+public class BGImagePickFragment extends BottomSheetDialogFragment implements BGImageRVAdapter.OnItemClickListener {
 
-    private BGImageRVAdapter adapter;
+    private BGImageRVAdapter presentAdapter, newAdapter;
     private SharedPreferenceHelper sharedPreferenceHelper;
+
     private CircularProgressIndicator progressBar;
+
+    private ArrayList<String> bgNameArrayList;
     private boolean warningShown = false;
 
     public BGImagePickFragment() {
@@ -64,9 +67,14 @@ public class BGImagePickFragment extends BottomSheetDialogFragment {
 
         progressBar = inflate.findViewById(R.id.imagePickProgressBar);
 
-        if (!warningShown && !isNetworkAvailable(requireContext())) {
-            Toast.makeText(requireContext(), "Please connect to the Internet", Toast.LENGTH_SHORT).show();
-            warningShown = true;
+        if (!isNetworkAvailable(requireContext())) {
+
+            inflate.findViewById(R.id.imagePickNewTitleTV).setVisibility(View.GONE);
+
+            if (!warningShown) {
+                Toast.makeText(requireContext(), "Please connect to the Internet", Toast.LENGTH_SHORT).show();
+                warningShown = true;
+            }
         }
 
         return inflate;
@@ -76,83 +84,41 @@ public class BGImagePickFragment extends BottomSheetDialogFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        adapter.setOnItemClickListener(uri -> {
-            final ProgressDialog dialog = ProgressDialog.show(requireContext(), "", "Please Wait....");
+        newAdapter.setOnItemClickListener(this);
 
-            final File f;
+        presentAdapter.setOnItemClickListener(this);
 
-            if (uri.toString().contains(
-                    (BuildConfig.DEBUG) ?
-                            "https://firebasestorage.googleapis.com/v0/b/quotes-debug-q.appspot.com" :
-                            "https://firebasestorage.googleapis.com/v0/b/quotes-q.appspot.com"
-            )) {
-
-                String fileName = String.valueOf(uri).split("%2F")[1].split("\\?")[0];
-
-                f = new File(requireContext().getFilesDir(), fileName);
-
-            } else {
-                f = new File(uri.toString().replace("file://", requireContext().getFilesDir().getPath()));
-            }
-
-            if (f.exists()) {
-                sharedPreferenceHelper.setBackgroundPath(f.getAbsolutePath());
-
-                dialog.dismiss();
-
-                Toast.makeText(requireContext(), "Background Set \n Applying Changes", Toast.LENGTH_LONG).show();
-
-                ((MainActivity) requireContext()).findViewById(R.id.constraintLayout).setBackground(Drawable.createFromPath(f.getAbsolutePath()));
-
-                dismiss();
-
-            } else {
-                StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("images").child(f.getName());
-
-                storageReference.getFile(f).addOnSuccessListener(taskSnapshot -> {
-
-                    sharedPreferenceHelper.setBackgroundPath(f.getAbsolutePath());
-
-                    dialog.dismiss();
-
-                    Toast.makeText(requireContext(), "Background Set \n Applying Changes", Toast.LENGTH_LONG).show();
-
-                    ((MainActivity) requireContext()).findViewById(R.id.constraintLayout).setBackground(Drawable.createFromPath(f.toString()));
-
-                    dismiss();
-
-                }).addOnFailureListener(exception -> {
-                    FirebaseCrashlytics.getInstance().recordException(exception);
-                    exception.printStackTrace();
-                    Toast.makeText(requireContext(), "Oops! Something went wrong!", Toast.LENGTH_LONG).show();
-                    dialog.dismiss();
-                });
-            }
-        });
     }
 
     private void setUpRecyclerView(View inflate) {
-        RecyclerView recyclerView = inflate.findViewById(R.id.imagePickRecyclerView);
-        adapter = new BGImageRVAdapter();
-        recyclerView.setAdapter(adapter);
+        bgNameArrayList = new ArrayList<>();
+        loadPresentImages(inflate);
+        loadNewImages(inflate);
+    }
 
-        recyclerView.setHasFixedSize(true);
+    private void loadPresentImages(View inflate) {
+
+        RecyclerView presentRecyclerView = inflate.findViewById(R.id.imagePickPresentRecyclerView);
+        presentAdapter = new BGImageRVAdapter();
+        presentRecyclerView.setAdapter(presentAdapter);
+
+        presentRecyclerView.setHasFixedSize(true);
 
         RecyclerView.LayoutManager layoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL) {
             @Override
             public void onLayoutCompleted(RecyclerView.State state) {
                 super.onLayoutCompleted(state);
-                if (state.getItemCount() >= 2 && MainActivity.bgDialog != null && MainActivity.bgDialog.isShowing())
+                if (state.getItemCount() > 0 && MainActivity.bgDialog != null && MainActivity.bgDialog.isShowing())
                     MainActivity.bgDialog.dismiss();
 
                 if (state.getItemCount() > 4)
                     progressBar.setVisibility(View.GONE);
             }
         };
-        recyclerView.setLayoutManager(layoutManager);
+        presentRecyclerView.setLayoutManager(layoutManager);
 
         final ArrayList<Uri> list = new ArrayList<>();
-        final ArrayList<String> bgArrayList = new ArrayList<>();
+        bgNameArrayList = new ArrayList<>();
 
         String[] files = requireContext().fileList();
 
@@ -165,22 +131,51 @@ public class BGImagePickFragment extends BottomSheetDialogFragment {
                         continue;
 
                     list.add(Uri.fromFile(file));
-                    bgArrayList.add(file.getName());
+                    bgNameArrayList.add(file.getName());
 
-                    if (adapter != null && getContext() != null) {
-                        adapter.submitList(list);
-                        adapter.notifyDataSetChanged();
+                    if (presentAdapter != null && getContext() != null) {
+                        presentAdapter.submitList(list);
+                        presentAdapter.notifyDataSetChanged();
                     }
                 }
+
             }
         } else {
             if (MainActivity.bgDialog != null && MainActivity.bgDialog.isShowing())
                 MainActivity.bgDialog.dismiss();
         }
 
-        if (bgArrayList.size() == 0 && MainActivity.bgDialog != null && MainActivity.bgDialog.isShowing()) {
+        if (bgNameArrayList.size() == 0 && MainActivity.bgDialog != null && MainActivity.bgDialog.isShowing()) {
             MainActivity.bgDialog.dismiss();
         }
+
+    }
+
+    private void loadNewImages(View inflate) {
+        RecyclerView newRecyclerView = inflate.findViewById(R.id.imagePickNewRecyclerView);
+        newAdapter = new BGImageRVAdapter();
+        newRecyclerView.setAdapter(newAdapter);
+
+        newRecyclerView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL) {
+            @Override
+            public void onLayoutCompleted(RecyclerView.State state) {
+                super.onLayoutCompleted(state);
+                if (state.getItemCount() > 0 && MainActivity.bgDialog != null && MainActivity.bgDialog.isShowing())
+                    MainActivity.bgDialog.dismiss();
+
+                if (state.getItemCount() > 4)
+                    progressBar.setVisibility(View.GONE);
+            }
+        };
+        newRecyclerView.setLayoutManager(layoutManager);
+
+        if (bgNameArrayList.isEmpty()) {
+            inflate.findViewById(R.id.imagePickPresentTitleTV).setVisibility(View.GONE);
+        }
+
+        final ArrayList<Uri> list = new ArrayList<>();
 
         if (isNetworkAvailable(requireContext())) {
             FirebaseStorage storage = FirebaseStorage.getInstance();
@@ -189,12 +184,12 @@ public class BGImagePickFragment extends BottomSheetDialogFragment {
             storageRef.listAll().addOnSuccessListener(listResult -> {
 
                 for (StorageReference item : listResult.getItems())
-                    if (!bgArrayList.contains(item.getName()))
+                    if (!bgNameArrayList.contains(item.getName()))
                         item.getDownloadUrl().addOnSuccessListener(uri -> {
                             list.add(uri);
-                            if (adapter != null && getContext() != null) {
-                                adapter.submitList(list);
-                                adapter.notifyDataSetChanged();
+                            if (newAdapter != null && getContext() != null) {
+                                newAdapter.submitList(list);
+                                newAdapter.notifyDataSetChanged();
                             }
                         });
             });
@@ -205,5 +200,60 @@ public class BGImagePickFragment extends BottomSheetDialogFragment {
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    @Override
+    public void onItemClick(Uri uri) {
+        final ProgressDialog dialog = ProgressDialog.show(requireContext(), "", "Please Wait....");
+
+        final File f;
+
+        if (uri.toString().contains(
+                (BuildConfig.DEBUG) ?
+                        "https://firebasestorage.googleapis.com/v0/b/quotes-debug-q.appspot.com" :
+                        "https://firebasestorage.googleapis.com/v0/b/quotes-q.appspot.com"
+        )) {
+
+            String fileName = String.valueOf(uri).split("%2F")[1].split("\\?")[0];
+
+            f = new File(requireContext().getFilesDir(), fileName);
+
+        } else {
+            f = new File(uri.toString().replace("file://", requireContext().getFilesDir().getPath()));
+        }
+
+        if (f.exists()) {
+            sharedPreferenceHelper.setBackgroundPath(f.getAbsolutePath());
+
+            dialog.dismiss();
+
+            Toast.makeText(requireContext(), "Background Set \n Applying Changes", Toast.LENGTH_LONG).show();
+
+            ((MainActivity) requireContext()).findViewById(R.id.constraintLayout).setBackground(Drawable.createFromPath(f.getAbsolutePath()));
+
+            dismiss();
+
+        } else {
+            StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("images").child(f.getName());
+
+            storageReference.getFile(f).addOnSuccessListener(taskSnapshot -> {
+
+                sharedPreferenceHelper.setBackgroundPath(f.getAbsolutePath());
+
+                dialog.dismiss();
+
+                Toast.makeText(requireContext(), "Background Set \n Applying Changes", Toast.LENGTH_LONG).show();
+
+                ((MainActivity) requireContext()).findViewById(R.id.constraintLayout).setBackground(Drawable.createFromPath(f.toString()));
+
+                dismiss();
+
+            }).addOnFailureListener(exception -> {
+                FirebaseCrashlytics.getInstance().recordException(exception);
+                exception.printStackTrace();
+                Toast.makeText(requireContext(), "Oops! Something went wrong!", Toast.LENGTH_LONG).show();
+                dialog.dismiss();
+            });
+        }
     }
 }
