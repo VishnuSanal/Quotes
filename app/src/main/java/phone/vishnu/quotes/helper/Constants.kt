fd3fa7d4844560894da951c950bbea29d999f69e
VishnuSanal <t.v.s10123@gmail.com>
2022-01-08 12:43:49 +0530

    ACRAErrorActivity

---
+++ app/src/main/java/phone/vishnu/quotes/helper/Constants.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2019 - 2022 Vishnu Sanal. T
+ *
+ * This file is part of Quotes Status Creator.
+ *
+ * Quotes Status Creator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+@file:Suppress("HardCodedStringLiteral")
+
+package phone.vishnu.quotes.helper
+
+object Constants {
+    const val ACRA_STACK_TRACE = "ACRA_STACK_TRACE"
+    const val ACRA_ANDROID_VERSION = "ACRA_ANDROID_VERSION"
+    const val ACRA_APP_VERSION_CODE = "ACRA_APP_VERSION_CODE"
+    const val ACRA_APP_VERSION_NAME = "ACRA_APP_VERSION_NAME"
+    const val ACRA_PACKAGE_NAME = "ACRA_PACKAGE_NAME"
+    const val ACRA_USER_APP_START_DATE = "ACRA_USER_APP_START_DATE"
+}
